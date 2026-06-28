3bbd37d99b3f12ab71e615eabfd078fdee86bba7
Vaibhav Solanke <vaibhavsolanke29@gmail.com>
2026-06-28 22:48:18 +0530

    Add agents for financial data retrieval and analysis

--- agents.py
+++ agents.py
@@ -1,37 +1,45 @@
 import os
 import yfinance as yf
 import feedparser
-
 from typing import TypedDict
-
 from langgraph.graph import StateGraph
 from langchain_groq import ChatGroq
 
+# Initializing llama 3 model through groq.
 llm = ChatGroq(
     model="llama-3.1-8b-instant",
-    api_key="GROQ_KEY"
+    api_key="GROQ_KEY"  # Add groq api key here
 )
 
+# Defining agents and agent workflow
 class AgentState(TypedDict):
 
     company:str
 
     ticker:str
 
+    # Company agent
     company_info:dict
 
+    # stock agent
     stock:dict
 
+    # News agent
     news:list
 
+    # finanace agent
     financials:dict
 
+    # Debt agent
     debt:dict
 
+    # Sentiment agent
     sentiment:str
 
+    # Decision agent
     decision:str
 
+# Company agent to get the company details from yahoo finance
 def company_agent(state):
 
     company = state["company"]
@@ -51,16 +59,17 @@ def company_agent(state):
     state["company_info"] = {
 
         "name":
-        quotes[0]["shortname"],
+        quotes[0]["shortname"], # Retrives companies short name from yahoo finance
 
         "exchange":
-        quotes[0]["exchange"]
+        quotes[0]["exchange"] # Retrieves stock exchange name from yahoo finance
     }
 
     return state
 
 import yfinance as yf
 
+# Stock agent to retrieve the details of company stock infromation from yahoo finance
 def stock_agent(state):
 
     ticker = state["ticker"]
@@ -76,6 +85,7 @@ def stock_agent(state):
 
     for date, row in history.iterrows():
 
+        # weekly stock history 
         week_close.append({
 
             "date": date.strftime("%Y-%m-%d"),
@@ -92,6 +102,7 @@ def stock_agent(state):
 
         })
 
+    # Company stock analysis current & historical.
     state["stock"] = {
 
         # Current Price
@@ -134,13 +145,14 @@ def stock_agent(state):
 
     return state
 
+# News agent to get company related news from google news
 def news_agent(state):
 
     ticker = state["ticker"]
 
     feed = feedparser.parse(
 
-        f"https://news.google.com/rss/search?q={ticker}"
+        f"https://news.google.com/rss/search?q={ticker}" # google news to get the company news
 
     )
 
@@ -156,6 +168,7 @@ def news_agent(state):
 
     return state
 
+# Financial agent to retrieve the publically available financial information about the company from yahoo finance
 def financial_agent(state):
 
     info = yf.Ticker(
@@ -163,19 +176,22 @@ def financial_agent(state):
     ).info
 
     state["financials"] = {
-
+        # Price to earnings
         "PE":
         info.get("trailingPE"),
-
+        
+        # earnings per share
         "EPS":
         info.get("trailingEps"),
 
+        # Revenue growth
         "RevenueGrowth":
         info.get("revenueGrowth")
     }
 
     return state
 
+# Debt agent to retrieve debt details of company from yahoo finance
 def debt_agent(state):
 
     info = yf.Ticker(
@@ -183,16 +199,18 @@ def debt_agent(state):
     ).info
 
     state["debt"] = {
-
+        # debt to equity in %
         "DebtToEquity":
         info.get("debtToEquity"),
 
+        # Debt to equity ratio
         "CurrentRatio":
         info.get("currentRatio")
     }
 
     return state
 
+# Sentiment agent to analyze companies recent news
 def sentiment_agent(state):
 
     prompt = f"""
@@ -209,13 +227,14 @@ def sentiment_agent(state):
     Negative
 
     """
-
+    # Giving prompt to llm
     response = llm.invoke(prompt)
 
     state["sentiment"] = response.content
 
     return state
 
+# Decision agent to generte detailed short report in markdown format
 def decision_agent(state):
 
     prompt = f"""
@@ -268,51 +287,62 @@ def decision_agent(state):
     Detailed reasoning.
     """
 
+    # passing the outputs of all agents to llm to generate report
     response = llm.invoke(prompt)
 
     state["decision"] = response.content
 
     return state
 
+# Creating workflow by using langgraph
 workflow = StateGraph(AgentState)
 
+# company node
 workflow.add_node(
     "company",
     company_agent
 )
 
+# stock node
 workflow.add_node(
     "stock",
     stock_agent
 )
 
+# news node
 workflow.add_node(
     "news",
     news_agent
 )
 
+# Financial agent node
 workflow.add_node(
     "financial",
     financial_agent
 )
 
+# Debt agent node
 workflow.add_node(
     "debt",
     debt_agent
 )
 
+# sentiment agent node
 workflow.add_node(
     "sentiment",
     sentiment_agent
 )
 
+# decision agent node
 workflow.add_node(
     "decision",
     decision_agent
 )
 
+# Defining entry point of workflow
 workflow.set_entry_point("company")
 
+# Connecting all the agents.
 workflow.add_edge(
     "company",
     "stock"
