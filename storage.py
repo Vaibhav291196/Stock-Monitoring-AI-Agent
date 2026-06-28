557ce7e00a19fa41380e146fd98c9dcd7686d1b6
Vaibhav Solanke <vaibhavsolanke29@gmail.com>
2026-06-28 23:04:54 +0530

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,8 +1,11 @@
-WATCHLIST = [
+# watchlist to store the company names
+WATCHLIST = 
+[
     "Apple",
     "Microsoft",
     "Tesla",
     "Nvidia"
 ]
 
+# Initializing reports list to store generated reports or company information at each instance of information retrieval cycle
 REPORTS = []
