857f629a36af1d1f16b84abd80942e4377d041ed
Vaibhav Solanke <vaibhavsolanke29@gmail.com>
2026-06-28 22:54:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,26 +8,30 @@ from storage import REPORTS
 from fastapi.responses import Response
 from storage import WATCHLIST, REPORTS
 
+# Initialize fast api
 app = FastAPI()
 
+# Addition of company to watchlist
 @app.post("/watchlist/{company}")
 def add_company(company: str):
 
     if company not in WATCHLIST:
         WATCHLIST.append(company)
 
+        # Return success message to user and display new watchlist to user
         return {
             "status": "success",
             "message": f"{company} added to watchlist",
             "watchlist": WATCHLIST
-        }
-
+        } 
+    # Give warning if company name already exists
     return {
         "status": "warning",
         "message": f"{company} already exists",
         "watchlist": WATCHLIST
     }
 
+# Removing company name from watchlist
 @app.delete("/watchlist/{company}")
 def remove_company(company: str):
 
@@ -35,18 +39,21 @@ def remove_company(company: str):
 
         WATCHLIST.remove(company)
 
+        # Return sucess message and display new list to user
         return {
             "status": "success",
             "message": f"{company} removed from watchlist",
             "watchlist": WATCHLIST
         }
 
+    # if company not found return error message
     return {
         "status": "error",
         "message": f"{company} not found",
         "watchlist": WATCHLIST
     }
 
+# Returning generated report back to user by get command of fast api.
 scheduler.start()
 @app.get("/reports")
 def reports():
